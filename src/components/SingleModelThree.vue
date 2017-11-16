6bbf9f9c26c9642001102008abc941ddc11cdd44
STAFF\zhangwei36 <1004695936@qq.com>
2017-11-16 15:26:02 +0800

    单体模式： 模块化、模拟静态变量、惰性单例

--- src/components/SingleModelThree.vue
+++ src/components/SingleModelThree.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        3
+        <p style="color: salmon; padding: 5px 0 5px 8px">结果F12查看控制台输出</p>
+        <img src="../assets/singlemodelthree.png" alt="">
     </div>
 </template>
 
@@ -10,6 +11,39 @@
         components: {},
         data () {
             return {}
+        },
+        created(){
+
+//            匿名函数执行完之后再返回了一个函数
+            const LazySingle = (function(){
+                let instance = null;
+                function single(){
+                    return {
+                        name : 'giraffe',
+                        sayName : function (i) {
+//                            调用单体内部的变量要用this
+                            console.log(this.name);
+                            this.name = i;
+                        }
+                    }
+                }
+                return function () {
+//                    惰性单例如果是首次创建才走if
+//                    虽然最下面两次调用了LazySingle()，但是1只会在控制台输出一次
+                    if(!instance){
+//                        惰性生成单例
+                        instance = single();
+                        console.log(1)
+                    }
+                    return instance
+                }
+            })();
+//            LazySingle是个立即执行函数的返回值，但是返回的是个函数，
+//            当此函数再次执行的时候才会真正返回一个单体对象，
+//            所以叫做惰性单体，
+//            而不是一上来就通过匿名函数创建好的单体
+            LazySingle().sayName('newName');
+            console.log(LazySingle().name)
         }
     }
 </script>
--- src/components/SingleModelTwo.vue
+++ src/components/SingleModelTwo.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        2
+        <p style="color: salmon; padding: 5px 0 10px 10px">结果F12查看控制台输出</p>
+        <img src="../assets/singlemodeltwo.png" alt="">
     </div>
 </template>
 
@@ -10,6 +11,36 @@
         components: {},
         data () {
             return {}
+        },
+        created(){
+//            匿名函数立即执行
+            const config = (function(){
+//                静态变量都要大写
+                const self = {
+                    URL : 'http://jr.sina.com.cn',
+                    PORT : '8080',
+                    MIN_NUM : 1,
+                    MAX_NUM : 10000,
+                    COUNT : 100
+                };
+//                return 返回的一个单体
+                return {
+//                    取值器
+                    getSelf : function (name) {
+                        return self[name] ? self[name] : null
+                    },
+//                    赋值器
+                    setSelf : function (name,value) {
+                        return self[name] ? self[name] = value : null
+                    }
+                }
+            })();
+//            取值URL
+            console.log(config.getSelf('URL'));
+//            赋值器赋值新端口号
+            config.setSelf('PORT','8888');
+//            取值新端口号
+            console.log(config.getSelf('PORT'))
         }
     }
 </script>
